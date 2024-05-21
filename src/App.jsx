73f791a8146900bd17793771c2895c8a9da671df
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-05-21 10:18:20 -0300

    Link de navegação entre paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,24 +7,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a className="logo" href="https://rogeriocordeiro.github.io/my-page/" target="_blank">
+          <h1>Meu repositório</h1>
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Minha Pagina Inicial</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Contador é {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Click em meu repositório para conhecer um pouco mais!
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Estou trabalhando para trazer muitas novidades!
       </p>
     </>
   )
