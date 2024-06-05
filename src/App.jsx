e8925140653e74a4a47c6a1d48a01626de492866
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-06-05 14:33:20 -0300

    Personalizando a pagina inicial.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import React from 'react'
+import NavBar from './components/NavBar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-        <a className="logo page" href="https://rogeriocordeiro.github.io/my-page/">
-          <h1>Minha Página</h1>
-        </a>
-        <a className="logo repo" href="https://github.com/RogerioCordeiro?tab=repositories" target='blank'>
-          <h1>Meu repositório</h1>
-        </a>
-
-      <h1>Bem vindo(a) a homepage!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Contador é {count}
-        </button>
-        <p>
-          Click em meu repositório para conhecer um pouco mais!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Estou trabalhando para trazer muitas novidades!
-      </p>
-    </>
+    <React.Fragment>
+      <NavBar />
+    </React.Fragment>
   )
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+function NavBar() {
+
+    return (
+<nav>
+    <div>
+        <div>
+            <a href="#"><img src="RACS" alt="logo-img" /></a>
+        </div>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Home</a></li>
+        </ul>
+        <div>
+            <a href="#">Sign In</a>
+            <a href="#">Register</a>
+        </div>
+    </div>
+</nav>
+    )
+}
+
+export default NavBar
