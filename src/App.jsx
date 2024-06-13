40c2c277cd8d8c928213e6c3b4276365ff43fefe
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-06-13 11:22:20 -0300

    Realiazdo alguns ajustes na ordem dos elemetos e texto da página inicial!

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
-        <a className="logo page" href="https://rogeriocordeiro.github.io/my-page/">
-          <h1>Minha Página</h1>
-        </a>
-        <a className="logo repo" href="https://github.com/RogerioCordeiro?tab=repositories" target='blank'>
-          <h1>Meu repositório</h1>
-        </a>
-
-      <h1>Bem vindo(a) a homepage!</h1>
+      <h1>Bem vindo(a)!</h1>
+      <a className="page" href="https://rogeriocordeiro.github.io/my-page/">
+        <p>Minha Página</p>
+      </a>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Contador é {count}
-        </button>
         <p>
           Click em meu repositório para conhecer um pouco mais!
         </p>
+        <a className="repo" href="https://github.com/RogerioCordeiro?tab=repositories" target='blank'>
+          <p>Meu repositório</p>
+        </a>
       </div>
       <p className="read-the-docs">
         Estou trabalhando para trazer muitas novidades!
