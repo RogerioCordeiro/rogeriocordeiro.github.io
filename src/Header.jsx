55cf3d6421d5dc90918e378c3d2885a24b731f01
RogerioCordeiro <rocordeirosilva@gmail.com>
2024-06-14 11:14:30 -0300

    Incluido o menu Hamburger e melhorias no logo da página.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,18 @@
 import logo from './assets/rogerioCordeiro.svg'
+import logoR from './assets/logoRo.svg'
 import './Header.css'
+import HamburgerMenu from './HamburgerMenu.jsx'
 
 function Header() {
     return(
         <header className="logo">
-            <a href="#" className="logo internal">
-                <img src={logo} alt="Rogerio Cordeiro" />
+            <a href="#" className="logoInternal">
+                <img src={logoR} alt="R" className='logoRo'/>
+                <img src={logo} alt="Rogerio Cordeiro" className='logoRoCor' />
             </a>
+            <div className='menu'>
+                <HamburgerMenu></HamburgerMenu>
+            </div>
         </header>
     )
 }
